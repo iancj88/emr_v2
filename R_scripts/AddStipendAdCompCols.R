353a3443dfaf8be8ebfb8c578f1a7d41c13bba1c
iancj88 <iancj88@gmail.com>
2017-11-27 08:03:20 -0700

    Added skeleton of Stipend/Ad-Comp column

--- R_scripts/AddStipendAdCompCols.R
+++ R_scripts/AddStipendAdCompCols.R
@@ -9,3 +9,5 @@ AddAdCompStipendCols <- function(df,
   adcmp_cols <- c(gid_col_name, key_date_col_name, date_col_name, assgn_sal_col_name)
   adcmp_df <- df[adcmp_indx, one_]
 }
+
+test <- read_file(file = "C:\Users\d66x816\AppData\Local\Temp\Banner Human Resources Forms 8.11.zip\payroll\ptvlcsv.html")
